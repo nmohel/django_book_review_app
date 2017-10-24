286017b2dcb0ef9235770add678ddaeaaa9cf40f
Nicole Ohel <nicole.ohel@gmail.com>
2017-10-24 13:22:09 -0700

    Added Create book and review functionality and html/css. Also updated database/index page so that created_at and updated_at are type datetime instead of just date, and recent reviews are displayed in correct order

--- apps/book_review/models.py
+++ apps/book_review/models.py
@@ -2,16 +2,35 @@ from __future__ import unicode_literals
 from django.db import models
 from ..login_reg.models import User
 
+class BookManager(models.Manager):
+    def validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 1:
+            errors['title_len'] = "Title cannot be blank"
+        elif Book.objects.filter(title=postData['title']):
+            errors['title_exist'] = postData['title'] + " is already on the site! Please add your review to existing book."
+        
+        if len(postData['author_new']) < 1:
+            if 'author_list' not in postData:
+                errors['no_author'] = "Please choose an author or add your own."
+
+        if len(postData['review_text']) < 1 :
+            errors['review_len'] = "Review cannot be blank"
+        
+        return errors
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = BookManager()
 
 class Review(models.Model):
     text = models.TextField()
     rating = models.IntegerField(null=True)
     writer = models.ForeignKey(User, related_name="reviews")
     book = models.ForeignKey(Book, related_name="reviews")
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- apps/book_review/views.py
+++ apps/book_review/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Book, Review
 from ..login_reg.models import User
+from django.contrib import messages
 
 def index(request):
     if 'user_id' in request.session:
         context = {
             'current_user': User.objects.get(id=request.session['user_id']),
             'all_books': Book.objects.all(),
-            'recent_reviews': Review.objects.order_by('created_at')[0:3],
+            'recent_reviews': Review.objects.order_by('-created_at')[0:3],
             'five': [1,2,3,4,5]
         }
         return render(request,'book_review/index.html', context)
@@ -15,9 +16,32 @@ def index(request):
         return redirect('/')
 
 def new(request):
-    return render(request, 'book_review/new.html')
+    if 'user_id' in request.session:
+        context = {
+            'ids_authors_list': Book.objects.raw("SELECT DISTINCT book_review_book.id, book_review_book.author FROM book_review_book")
+        }
+        return render(request, 'book_review/new.html', context)
+    else:
+        return redirect('/')
 
 def create_book(request):
+    if request.method == 'POST':
+        errors = Book.objects.validator(request.POST)
+        if len(errors):
+            for tag, error in errors.iteritems():
+                messages.error(request, error, extra_tags=tag)
+        else:
+            if request.POST['author_new'] > 1:
+                author = request.POST['author_new']
+            else:
+                author = request.POST['author_list']
+
+            user = User.objects.get(id = request.session['user_id'])
+            book = Book.objects.create(title=request.POST['title'], author=author)
+            review = Review.objects.create(text=request.POST['review_text'], rating=int(request.POST['rating']), writer=user, book=book)
+            book_url = '/books/' + str(book.id)
+            return redirect(book_url)
+    
     return redirect('/books/add')
 
 def add_review(request):
